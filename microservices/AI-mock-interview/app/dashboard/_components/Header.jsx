6587f78703205033ec2b7f806e37bdd24c6e8d1e
Altafalam3 <altaf.alam0032@gmail.com>
2024-10-23 16:53:26 +0530

    Edge case fix

--- microservices/AI-mock-interview/app/dashboard/_components/Header.jsx
+++ microservices/AI-mock-interview/app/dashboard/_components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
       {/* <Image src={'/logo.svg'} width={160} height={100} alt='logo' /> */}
-      <h6>JobLink</h6>
+      <h6>Jflow</h6>
       <ul className='hidden md:flex gap-6'>
 
         <li onClick={() => getRoutLink('/dashboard')} className={`hover:text-primary hover:font-bold transition-all cursor-pointer  ${path == '/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
